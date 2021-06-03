28fbd7280275d07526d94a1cb30f16e712219454
ircloud <realtimecompute.luojie@gmail.com>
2021-06-03 15:06:08 +0800

    feat: Kafka Producer

---
+++ pyflink13-example/project/common/Kafka_Producer.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# Name:         KafkaProducer
+# Description:
+# Author:       orange
+# Date:         2021/6/3
+# -------------------------------------------------------------------------------
+
+from kafka import KafkaProducer
+
+
+class KafkaMsgProducer:
+
+    def __init__(self, server, topic):
+        self._server = server
+        self.producer = None
+        self.topic = topic
+
+    def connect(self):
+        if self.producer is None:
+            producer = KafkaProducer(bootstrap_servers=self)
+            self.producer = producer
+
+    def close(self):
+        if self.producer is not None:
+            self.producer.close()
+            self.producer = None
+
+    def send(self, msg):
+        if self.producer is not None:
+            if not isinstance(msg, bytes):
+                msg = msg.eccode("utf-8")
+            self.producer.send(topic=self.topic, value=msg)
+
+
+def get_kafka_producer(port, topic):
+    producer = KafkaMsgProducer("localhost:%d" % port, topic)
+    producer.connect()  # 建立连接
+    return producer
